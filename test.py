07b87738b5a215a790fbb0a93f7a3be1dc2da548
orde <ordehiller@hotmail.com>
2019-08-05 23:44:47 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-open("http://www.ruby-lang.org/en") {|f|
-  f.each_line {|line| p line}
-  p f.base_uri         # <URI::HTTP:0x40e6ef2 URL:http://www.ruby-lang.org/en/>
-  p f.content_type     # "text/html"
-  p f.charset          # "iso-8859-1"
-  p f.content_encoding # []
-  p f.last_modified    
-}
